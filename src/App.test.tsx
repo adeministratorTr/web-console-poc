9c786d368fa689bdb6c849f1e1e24d2da5885a85
adeministratorTr <adembavas@gmail.com>
2022-09-06 22:07:26 +0200

    Update App test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>
+  render(
+    // @TODO: Update test when there is new/valid store
+    // <Provider store={store}>
+    <App />
+    // </Provider>
   );
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
+  expect(screen.getByTestId('App')).toBeVisible();
 });
