da7d0a0936e106bbf68e5d149854b5eeea8d4274
adeministratorTr <adembavas@gmail.com>
2022-09-07 23:04:12 +0200

    Add store to Home page test

--- src/pages/Home/Home.test.tsx
+++ src/pages/Home/Home.test.tsx
@@ -1,10 +1,16 @@
-import { render, screen } from '@testing-library/react';
-
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { store } from 'app/store';
 import Home from './index';
 
 describe('pages/Home', () => {
-  it('should render default', () => {
-    render(<Home />);
+  it('should render default', async () => {
+    render(
+      <Provider store={store}>
+        <Home />
+      </Provider>
+    );
+    await waitForElementToBeRemoved(screen.getByTestId('Loading'));
     expect(screen.getByTestId('Home')).toBeVisible();
   });
 });
