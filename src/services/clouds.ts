481d3482a5088839779d2c58a4ea37f4e956e1b6
adeministratorTr <adembavas@gmail.com>
2022-09-07 19:20:06 +0200

    Add clouds service

---
+++ src/services/clouds.ts
@@ -0,0 +1,32 @@
+const API_URL = process.env.REACT_APP_API_URL;
+const API_VERSION = process.env.REACT_APP_API_VERSION;
+
+export type TCloud = {
+  cloud_description: string;
+  cloud_name: string;
+  geo_latitude: number;
+  geo_longitude: number;
+  geo_region: string;
+};
+
+export type TCloudPlatforms = {
+  clouds: TCloud[];
+  error?: {
+    message: string;
+    more_info: string;
+    status: number;
+  };
+  message?: string;
+};
+
+export async function getCloudPlatforms(): Promise<TCloudPlatforms> {
+  try {
+    const response: TCloudPlatforms = await fetch(`${API_URL}/${API_VERSION}/clouds`)
+      .then((result) => result.json())
+      .then((resp) => resp);
+
+    return response;
+  } catch (e) {
+    throw new Error(String(e)); // it should be logged in real life
+  }
+}
