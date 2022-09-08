796e80662c58dd7178eaa92ed74418abc51b3ac2
adeministratorTr <adembavas@gmail.com>
2022-09-08 22:27:00 +0200

    Update Home page error handler

--- src/pages/Home/Home.test.tsx
+++ src/pages/Home/Home.test.tsx
@@ -1,7 +1,33 @@
 import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import { store } from 'app/store';
+import { fetchClouds } from 'reducers/clouds';
+import { createMockStore } from 'utils/testUtils';
+
 import Home from './index';
+
+const {
+  reduxThunkTester: { clearActionHistory },
+  store
+} = createMockStore();
+
+beforeEach(() => clearActionHistory());
+
+const sampleClouds = [
+  {
+    cloud_description: 'Africa, South Africa - Amazon Web Services: Cape Town',
+    cloud_name: 'aws-af-south-1',
+    geo_latitude: 10,
+    geo_longitude: -10,
+    geo_region: 'africa'
+  },
+  {
+    cloud_description: 'Africa, South Africa - Azure: South Africa North',
+    cloud_name: 'azure-south-africa-north',
+    geo_latitude: -50,
+    geo_longitude: -100,
+    geo_region: 'africa'
+  }
+];
 
 describe('pages/Home', () => {
   it('should render default', async () => {
@@ -10,7 +36,16 @@ describe('pages/Home', () => {
         <Home />
       </Provider>
     );
+    store.dispatch({ type: fetchClouds.fulfilled.toString(), payload: sampleClouds });
+
+    // user should see Loading component until action dispatched
+    expect(screen.getByTestId('Loading')).toBeVisible();
+
+    // when action dispatched successfully, user should not see any Loading component,
+    // and should see the List
     await waitForElementToBeRemoved(screen.getByTestId('Loading'));
     expect(screen.getByTestId('Home')).toBeVisible();
+    expect(screen.queryByTestId('Error')).not.toBeInTheDocument();
+    expect(screen.getByTestId('List')).toBeVisible();
   });
 });
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -33,7 +33,9 @@ export default function Home() {
     <div data-testid="Home">
       <h1>Home Page</h1>
       {initialCloudState.status === 'loading' && <Loading />}
-      {initialCloudState.status === 'fail' && <p>Ooops. Something went wrong</p>}
+      {initialCloudState.status === 'fail' && (
+        <p data-testid="Error">Ooops. Something went wrong</p>
+      )}
       {initialCloudState.status === 'success' && initialCloudState.clouds.length > 0 && (
         <p data-testid="List">{initialCloudState.clouds[0].cloud_description}</p>
       )}
